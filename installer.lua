84f446a1fc7c8a7fe9dc09e133cf2e72c63d3fbb
Lfrx <113234860+Lfrxxofficial@users.noreply.github.com>
2025-12-30 17:04:09 +0100

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,1 +1,6 @@
-downloadserver = 
+downloadserver = "https://raw.githubusercontent.com/Lfrxxofficial/XenOS/refs/heads/main/1.0/"
+term.setBackgroundColor(colors.green)
+term.setCursorPos(1,1)
+term.write("Welcome To the installer of XenOs")
+term.setCursorPos(1,3)
+term.write("Download Server is "..downloadserver)
